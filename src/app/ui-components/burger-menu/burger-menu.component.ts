9abd3d5838cb6b7290bd95e845c1af5fd94f45a8
MRinshad <rinshadmorayur09@gmail.com>
2024-08-16 01:12:58 +0530

    created sub menu in product section using expansion panel

--- src/app/ui-components/burger-menu/burger-menu.component.ts
+++ src/app/ui-components/burger-menu/burger-menu.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import {
+  ButtonDirective,
+  CardBodyComponent,
+  CardComponent,
+  CollapseDirective,
+  TextColorDirective
+} from '@coreui/angular';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-burger-menu',
   standalone: true,
-  imports: [RouterLink,NgClass],
+  imports: [
+    RouterLink,
+    NgClass,
+    ButtonDirective,
+    CollapseDirective,
+    TextColorDirective,
+    CardComponent,
+    CardBodyComponent,
+    NgIf,
+    MatExpansionModule],
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.css'
 })
 export class BurgerMenuComponent {
   constructor(public router: Router){}
+  panelOpenState = signal(false);
+  visible = [false, false];
+
+  toggleCollapse(id: number): void {
+    this.visible[id] = !this.visible[id];
+  }
 }
